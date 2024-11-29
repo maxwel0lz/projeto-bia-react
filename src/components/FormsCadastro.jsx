b8497faec25018824ad82cc56f317ec88afff01a
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-11-29 01:54:21 -0300

    Login totalmente responsivo

--- src/components/FormsCadastro.jsx
+++ src/components/FormsCadastro.jsx
@@ -1,20 +1,40 @@
 import { ArrowBigDown, LockKeyhole, MailIcon, UserPlus } from "lucide-react"
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 
 const inputs = "flex gap-5 bg-gray-400 py-5 px-5 rounded-[4px] bg-opacity-25 w-[52%] items-center"
 function FormsCadastro(){
+
+    const [largur, setLargura] = useState(window.innerWidth);
+    
+    useEffect(() => {
+        // Função para atualizar a largura
+        const atualizarLargura = () => {
+        setLargura(window.innerWidth);
+        };
+
+        // Adicionar o listener para o evento resize
+        window.addEventListener("resize", atualizarLargura);
+
+        // Limpar o listener ao desmontar o componente
+        return () => {
+        window.removeEventListener("resize", atualizarLargura);
+        };
+    }, []);
+    
+    const st = largur < 650? "w-full":"w-[52%]"
+
     return(
         <>
             <div className="flex flex-col gap-3 items-center">
-                <h1 className="text-pink-500 font-serif text-5xl">Crie sua conta</h1>
-                <h2 className="font-serif text-black flex flex-row items-center " >Preencha seus dados <ArrowBigDown  size={20} strokeWidth={1} /></h2>
+                <h1 className="flex text-center whitespace-nowrap text-pink-500 font-serif text-5xl">Crie sua conta</h1>
+                <h2 className="font-serif text-center text-black flex flex-row items-center " >Preencha seus dados <ArrowBigDown  size={20} strokeWidth={1} /></h2>
             </div>
             <form className="flex flex-col items-center gap-16 w-full" action="">
                 <div className="flex flex-col gap-6 font-mono w-full items-center">
-                    <span className={inputs}><UserPlus color="#6b7280" size={22}/><input className="bg-transparent outline-none text-gray-60000 placeholder:text-gray-500 w-full h-full " type="text" placeholder="Nome" /></span>
-                    <span className={inputs}><MailIcon color="#6b7280" size={22}/><input className="bg-transparent outline-none text-gray-600 placeholder:text-gray-500 w-full h-full " type="email" placeholder="Email" /></span>
-                    <span className="flex gap-5 bg-gray-400 py-5 px-5 rounded-[4px] bg-opacity-25 w-[52%] items-center "><LockKeyhole color="#6b7280" size={22}/><input className=" bg-transparent outline-none text-gray-600 placeholder:text-gray-500 w-full h-full " type="password" placeholder="Senha" /></span>
+                    <span className={`${inputs} ${st}`}><UserPlus color="#6b7280" size={22}/><input className="bg-transparent outline-none text-gray-60000 placeholder:text-gray-500 w-full h-full " type="text" placeholder="Nome" /></span>
+                    <span className={`${inputs} ${st}`}><MailIcon color="#6b7280" size={22}/><input className="bg-transparent outline-none text-gray-600 placeholder:text-gray-500 w-full h-full " type="email" placeholder="Email" /></span>
+                    <span className={`${inputs} ${st}`}><LockKeyhole color="#6b7280" size={22}/><input className=" bg-transparent outline-none text-gray-600 placeholder:text-gray-500 w-full h-full " type="password" placeholder="Senha" /></span>
                 </div>
                 <button className="bg-pink-500 text-white rounded-full px-12" type="submit">ENTRAR</button>
             </form>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,44 +1,88 @@
-import { ArrowBigDown, LockKeyhole, MailIcon, UserPlus} from "lucide-react";
-import React, { useState } from "react";
+//import { ArrowBigDown, LockKeyhole, MailIcon, UserPlus} from "lucide-react";
+import React, { useEffect, useState } from "react";
 import logoReduzida from "../assets/logo-reduzia.svg"
 import FormsLogin from "../components/FormsLogin";
 import FormsCadastro from "../components/FormsCadastro";
-const inputs = "flex gap-5 bg-gray-400 py-5 px-5 rounded-[4px] bg-opacity-25 w-[52%] items-center"
-const [tipoForm, setTipoForm] = useState(0)
-const logar = () => {tipoForm == 0? setTipoForm(1) : setTipoForm(0)}
-function Login(){
 
+
+
+function Login(){
+    //const inputs = "flex gap-5 bg-gray-400 py-5 px-5 rounded-[4px] bg-opacity-25 w-[52%] items-center"
+    const  [tipoForm, setTipoForm] = useState(0)
+    const logar = () => {tipoForm == 0? setTipoForm(1) : setTipoForm(0)}
+    
+    const [largura, setLargura] = useState(window.innerWidth);
+
+    useEffect(() => {
+        // Função para atualizar a largura
+        const atualizarLargura = () => {
+        setLargura(window.innerWidth);
+        };
+
+        // Adicionar o listener para o evento resize
+        window.addEventListener("resize", atualizarLargura);
+
+        // Limpar o listener ao desmontar o componente
+        return () => {
+        window.removeEventListener("resize", atualizarLargura);
+        };
+    }, []);
+
+     const stSession = largura < 650? "flex-col items-center":"flex-row "
+     const stDivLeft = largura < 650? "hidden":""
+     const stForm = largura < 400? "w-[80%]":"w-[58%]"
+     
     return(
-        <section className="flex flex-row w-full ">
+        <section className={`flex ${stSession} w-full`}>
             {/* Apresentação */}
-            <div className="flex flex-col bg-pink-500 h-screen w-[42%] justify-around items-center ">
+            <div className={`flex flex-col bg-pink-500 h-screen w-[42%] justify-around items-center ${stDivLeft} `}>
                 <div className="flex items-center justify-center bg-white w-full h-20">
                     <img className="w-44 h-full" src={logoReduzida} alt="" />
                 </div>
-                <div className="flex flex-col gap-5 text-white font-serif text-center my-10">
-                    <h1 className=" text-5xl  font-bold">Bem-vindo <br/> de volta!</h1>
-                    <h2 className=" text-2xl   ">Acesse sua conta <br /> agora e aproveite!</h2>
+                <div className="flex flex-col items-center  gap-5 text-white font-serif text-center my-10 w-[52%] max-md:w-full">
+                    <h1 className=" text-5xl   font-bold">{tipoForm === 0? `Bem-vindo de volta!`:"Vamos criar seu acesso?" }</h1>
+                    <h2 className=" text-2xl   ">{tipoForm === 0? "Clique em LOGIN e acesse sua conta":"Clique abaixo e cadastrar-se em nosso site!" }</h2>
+                    
                 </div>
                 <div className="flex flex-col font-serif font-bold gap-2 items-center ">
                     <button 
                         className="bg-white text-pink-500 font-bold hover:border-white hover:bg-pink-500 hover:text-white transition-colors rounded-full px-16"
                         onClick={logar}
+                        
                     >
-                        LOGIN
+                        {tipoForm === 0? "LOGIN" : "CADASTAR-SE"}
+                        
                     </button>
                     <a className="text-white hover:underline hover:text-white hover:cursor-pointer">esqueceu a senha?</a>
                 </div>
 
             </div>
-            
+            {largura < 650 &&
+                <div className="flex flex-row justify-center gap-5 items-center bg-pink-500 w-full h-16">
+                    <span className="flex flex-col justify-center">
+                        <h1 className=" text-lg  font-bold">{tipoForm === 0? `Ja possui conta?`:"Vamos criar seu acesso?" }</h1>
+                        <h2 className=" text-sm   ">{tipoForm === 0? "Clique em LOGIN e acesse sua conta":"Clique e cadastrar-se em nosso site!" }</h2>
+                    </span>
+                    <button 
+                        className="h-10 bg-white text-xs text-pink-500 font-bold hover:border-white hover:bg-pink-500 hover:text-white transition-colors rounded-lg py-1 px-4"
+                        onClick={logar}
+                        
+                    >
+                        {tipoForm === 0? "LOGIN" : "CADASTAR-SE"}
+                        
+                    </button>
+                </div>
+            }
+
             
             {/* Formulario */}
-            <div className="flex flex-col text-black bg-white h-screen w-[58%] items-center justify-center gap-12">  
-                <FormsLogin />
-                <FormsCadastro />
+            <div className={`flex flex-col text-black bg-white h-screen ${stForm} items-center justify-center gap-12`}>  
+                {tipoForm === 0? <FormsCadastro /> : <FormsLogin /> }
             </div>
         </section>
+        
     )
+    
 }
 
 export default Login;
