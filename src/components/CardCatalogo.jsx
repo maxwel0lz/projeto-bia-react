c0929bef9bf9d88621f2f425e3c4f348f22353af
maxwel0lz <134058339+maxwel0lz@users.noreply.github.com>
2024-12-03 22:58:50 -0300

    Catalogo finalizado

--- src/components/CardCatalogo.jsx
+++ src/components/CardCatalogo.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import imgservico1 from '../assets/esfoliacao.jpeg'
-function CardCatalogo({titulo,tipo, texto}) {
-  return (
-    <div className="font-card max-lg:flex-col  bg-opacity-50 flex flex-row justify-around items-center px-36 max-sm:px-0 my-3">
-            
-            <div className={`${tipo} max-lg:order-none w-[60%] max-md:w-[80%] flex justify-center items-center col-span-3`}>
-                <img className="w-[300px] object-contain  rounded-lg" src={imgservico1} alt="" />
-            </div>
 
-            <div className="flex flex-col items-start gap-3 w-[40%] max-lg:w-full max-lg:items-center  max-lg:text-justify text-left  col-span-2 p-10  rounded-lg shadow-right ">
-                <h3 className="text-pink-800 text-3xl  font-bold">Design fio a fio</h3>
-                <p className="text-wrap text-gray-600 text-lg">{texto}</p>
+function CardCatalogo({titulo,tipo, descricao, imagem}) {
+  return (
+    <div className="w-[80%] min-w-[50rem] max-md:min-w-full shadow-2xl rounded-lg font-card max-md:flex-col  bg-opacity-50 flex flex-row justify-center gap-10 max-md:gap-0 items-center px-24 max-sm:px-5">
+            
+            <div className={`order-${tipo}  max-lg:order-none   flex justify-center items-center  py-5`}>
+                <img className="shadow-xl w-[18rem] min-w-[8rem]  rounded-lg" src={imagem} alt="" />
+            </div>
+ 
+            <div className="flex flex-col text-left items-start gap-5 w-1/2 max-md:p-3   max-md:w-full  max-lg:items-center  max-lg:text-justify    ">
+                <h3 className="text-shadow-sm text-pink-800 text-3xl  font-bold max-md:text-center">{titulo}</h3>
+                <p className=" text-gray-600 text-lg text-shadow-md max-sm:text-base">{descricao}</p>
 
                 <button 
-                    className="font-semibold leading-none flex  text-gray-600 bg-transparent border border-pink-600 hover:bg-pink-600 hover:text-white hover:border-white transition duration-150 ease-in-out"
+                    className="shadow-lg font-semibold leading-none flex  text-gray-600 bg-transparent border border-pink-600 hover:bg-pink-600 hover:text-white hover:border-white transition duration-150 ease-in-out"
                     type="button"
                 >
-                    Saiba Mais
+                    AGENDAR
                 </button>
             </div>
         </div>
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import CardCatalogo from '../components/CardCatalogo'
-
+import imgservico1 from '../assets/esfoliacao.jpeg'
+import designSobraselha from '../assets/designSobranselha.jpeg'
 function Catalogo() {
     
   return (
-    <div className='flex flex-col items-center bg-gray-300'>
-      <CardCatalogo titulo={"Design de sobrancelha"} texto={"text-left"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} tipo={"flex-row-reverse"} texto={"text-center"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} texto={"text-left"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} tipo={"flex-row-reverse"} texto={"text-center"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} texto={"text-left"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} tipo={"flex-row-reverse"} texto={"text-center"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} texto={"text-left"} />
-      <CardCatalogo titulo={"Design de sobrancelha"} tipo={"flex-row-reverse"} texto={"text-center"} />
+    <div className='flex flex-col items-center bg-gray-300 max-sm:p-10 p-20'>
+      <CardCatalogo
+        imagem={designSobraselha}
+        titulo={"Design Personalizado"}
+        descricao={"Design Personalizado harmoniza as sobrancelhas de acordo com o que o seu rosto se adequar o Design de sobrancelhas é único e Personalizado de acordo com cada cliente"}
+      />
+      <CardCatalogo 
+        imagem={imgservico1}
+        titulo={"Hidragloss"} 
+        tipo={"1"} descricao={"Hidragloss é hidratação dos lábios através de ativos a base de ácido hialurônico ideal para lábios ressecados"}
+      />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} descricao={"text-left"} />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} tipo={"1"} descricao={"text-center"} />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} descricao={"text-left"} />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} tipo={"1"} descricao={"text-center"} />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} descricao={"text-left"} />
+      <CardCatalogo imagem={imgservico1}titulo={"Design de sobrancelha"} tipo={"1"} descricao={"text-center"} />
       
     </div>
   )
